25434579a03b61383d3e364d00271ac300d46cd3
Oscar <github@oscarb.se>
2016-02-16 11:29:42 +0100

    Different ways to identify button clicked

--- app/src/main/java/se/oscarb/differentlisteners/MainActivity.java
+++ app/src/main/java/se/oscarb/differentlisteners/MainActivity.java
@@ -18,6 +18,34 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Klickade på knappen!", Toast.LENGTH_SHORT).show();
     }
 
+    // Skilja på vilken knapp man klickade på med hjälp av tags...
+    public void runMethod2(View view) {
+        String tag = view.getTag().toString();
+        String message = "";
+        if(tag.equals("dog")) {
+            message = "Du klickade på en HUND!";
+        } else if(tag.equals("cat")) {
+            message = "Du klickade på en KATT!";
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    // Skilja på vilken knapp man klickade på med hjälp av ID...
+    public void runMethod3 (View view) {
+        String message;
+
+        // if (view.getId() == R.id.special_button_1) ...
+
+        switch(view.getId()) {
+            case R.id.special_button_1: message = "Speciell knapp 1"; break;
+            case R.id.special_button_2: message = "Speciell knapp 2"; break;
+            default: message = "Ingen speciell knapp!";
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 
 
 }
